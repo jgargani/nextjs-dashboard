87ae9a58e1cb320c6b5a1b6bac6bef97c9adf4a3
Jim Gargani <jgargani@comcast.net>
2024-05-08 15:55:33 -0400

    Updated

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -19,6 +19,7 @@ export default async function Page() {
   } = await fetchCardData();
 
   return (
+    
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
