ab2107902651f3b6c63459c6f150f2516b6a87c5
Jim Gargani <jgargani@comcast.net>
2024-05-08 10:56:21 -0400

    modified:   app/ui/dashboard/latest-invoices.tsx 	modified:   app/ui/dashboard/revenue-chart.tsx

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -9,7 +9,11 @@ import { Revenue } from '@/app/lib/definitions';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default function RevenueChart({ revenue }: { revenue: Revenue[] }) {
+export default async function RevenueChart({
+  revenue,
+}: {
+  revenue: Revenue[];
+}) {
   const chartHeight = 350;
   // NOTE: comment in this code when you get to this point in the course
 
