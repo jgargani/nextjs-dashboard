5702352f8f0bf9afda0f94f2591775579b060853
Jim Gargani <jgargani@comcast.net>
2024-05-17 10:39:59 -0400

    Updated

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,6 +9,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
+    console.log(term);
+
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
